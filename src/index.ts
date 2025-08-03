8a6c1e43dcec8133a0e96f360affe7d50c7ca92b
Rodrigo <rodrigoalfonzo97@gmail.com>
2025-08-03 01:14:33 -0400

    Flash commit: Solo descomenté la funcion de helmet y de morgan

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@
 import 'dotenv/config'; // Cargar variables de entorno desde .env
 import express from 'express';
 import cors from 'cors';
-//import helmet from 'helmet'; // Para seguridad
-//import morgan from 'morgan'; // Para logging
+import helmet from 'helmet'; // Para seguridad
+import morgan from 'morgan'; // Para logging
 import { pacientesRouter } from './routers/pacientes.routers';
 import { platillosRouter } from './routers/platillos.routers'; // Asegúrate de importar platillosRouter
 
@@ -18,8 +18,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-//app.use(helmet());
-//app.use(morgan('dev')); // Logging en desarrollo  
+app.use(helmet());
+app.use(morgan('dev')); // Logging en desarrollo  
 
 
 // Ruta de ejemplo
